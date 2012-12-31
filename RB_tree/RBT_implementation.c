8ca1fa2cf0d7b8393a273e4f4acfb454597e901e
Neil Sant Gat aka 'sexy lover boy <gatneil@gmail.com>
2012-12-30 22:53:40 -0500

    updated structs, constants, and interface; before implementing

--- RB_tree/RBT_implementation.c
+++ RB_tree/RBT_implementation.c
@@ -1,12 +1,18 @@
-/**************************************
- * Implementation of a red-black tree *
- * Neil Sant Gat                      *
- * Begun Sunday, December 30th, 2012  *
- **************************************/
+/***********************************************
+ * Implementation of a red-black tree          *
+ * Neil Sant Gat                               *
+ * Begun Sunday, December 30th, 2012           *
+ * thanks to Wikipedia for a great             *
+ * description of RB tree semantics            *
+ * http://en.wikipedia.org/wiki/Red_Black_tree *
+ ***********************************************/
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
+
+#define LEAF 0xDEADBEEF
+#define BRANCH 0xFEEDBEEF
 
 /****************
  * USER-DEFINED *
@@ -31,20 +37,31 @@ int int_compare(my_type *a, my_type *b) {
 /*************
  * INTERFACE *
  *************/
+typedef struct rb_node {
+  my_type *data;
+  int node_type;
+  struct rb_node *parent;
+  struct rb_node *left;
+  struct rb_node *right;
+} rb_node;
 
 typedef struct sexy_rb_tree {
-  my_type *root;
+  rb_node *root;
   int num_nodes;
 
   // returns -1 iff "a < b", 0 iff "a == b", 1 iff "a > b"
   int (*comp)(my_type *, my_type *);
 } sexy_rb_tree;
 
-// usual "insert, remove, and search" functions
-// all 3 rely on the passed in comparison function
+// usual "create, insert, remove, search, and free" functions
+// first 4 rely on the comparison function passed into create_rb()
+sexy_rb_tree *create_rb(int (*)(my_type *, my_type *));
 int insert_baby(my_type *, sexy_rb_tree *);
 int remove_baby(my_type *, sexy_rb_tree *);
 int search_baby(my_type *, sexy_rb_tree *);
+int free_rb(sexy_rb_tree *);
+
+// helper functions: DO NOT EXPOSE
 
 /***************
  * TEST SCRIPT *
